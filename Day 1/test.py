16dded9174932177fe47394ce65d2bd8e36934e9
CLaireChoi1209 <4clairechoi@gmail.com>
2023-01-06 22:29:04 +0900

    2

---
+++ Day 1/test.py
@@ -0,0 +1,13 @@
+print("안녕,파이썬")
+i=0
+print(i)
+print(2**2)
+print(2**3)
+apple=10
+orange=20
+print(orange+apple)
+print(orange-apple)
+print(orange*apple)
+print(orange/apple)
+print(orange//apple)
+print(orange%apple)
